7cdc31f84022c69b9f20ede8e41ec41fcd9a01bb
isarikaya <isarikayaa@outlook.com>
2021-07-04 18:13:57 +0300

    Site layout and pages created.

---
+++ src/lib/Header.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/">Anasayfa</a>
+        </li>
+        <li>
+            <a href="/about">Hakkımızda</a>
+        </li>
+        <li>
+            <a href="/contact">İletişim</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    ul{
+        display: flex;
+        list-style: none;
+    }
+    li{
+        margin-right: 10px;
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Header from "$lib/Header.svelte";
+</script>
+
+<Header />
+<slot />
