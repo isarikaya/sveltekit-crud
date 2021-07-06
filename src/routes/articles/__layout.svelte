e29cfd4aab7781683646cc5a3f96bdc57f31c63a
isarikaya <isarikayaa@outlook.com>
2021-07-06 16:06:51 +0300

    Multiple layouts.

---
+++ src/routes/articles/__layout.svelte
@@ -0,0 +1,11 @@
+<div class="container">
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+        max-width: 800px;
+        margin: 50px auto;
+        background-color: rgb(172, 172, 172);
+    }
+</style>
