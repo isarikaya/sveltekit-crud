8bb454d66476079fd43726facad74eb54491bebf
isarikaya <isarikayaa@outlook.com>
2021-07-06 17:43:38 +0300

    Post request contact form.

---
+++ src/routes/api/contact.js
@@ -0,0 +1,11 @@
+export const post = (request) => {
+    const formBody = JSON.parse(request.body)
+    const name = formBody.name
+    const email = formBody.email
+    return {
+        body: {
+            name,
+            email
+        }
+    }
+}
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,1 +1,47 @@
+<script>
+    let name = 'İbrahim',
+        email = 'isarikayaa@outlook.com',
+        message = '',
+        error = '';
+    const submitForm = async () => {
+        try {
+            const submit = await fetch("/api/contact", {
+                method: "POST",
+                body: JSON.stringify({
+                    name,
+                    email
+                })
+            })
+            const data = await submit.json()
+            message = data
+        } catch (err) {
+            error = err
+        }
+    }
+</script>
+
 <h1>İletişim</h1>
+
+{#if !message && !error}
+<form on:submit|preventDefault={submitForm}>
+    <label for="">
+        İsim:
+        <input type="text" name="name" bind:value={name}>
+    </label>
+    <label for="">
+        Email:
+        <input type="email" name="email" bind:value={email}>
+    </label>
+    <input type="submit" value="Submit">
+</form>
+{:else if message}
+<p>Merhaba {message.name} Bey bilgileriniz gönderildi.</p>
+{:else if error}
+<p>Form gönderilirken bir hata oluştu lütfen {email} ile iletişime geçin.</p>
+{/if}
+<style>
+    label {
+        display: block;
+        margin-bottom: 20px;
+    }
+</style>
