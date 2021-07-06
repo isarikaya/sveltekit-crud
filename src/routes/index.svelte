8da9dd8c72bde2d595b7ad0981d921077d8ee868
isarikaya <isarikayaa@outlook.com>
2021-07-06 16:38:08 +0300

    Endpoints and fetch posts.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,21 @@
+<script context="module">
+    export const load = async ({fetch}) => {
+        const response = await fetch("/api/post")
+        const data = await response.json()
+        console.log(data)
+
+        return {
+            props: {
+                data
+            }
+        }
+    }
+</script>
+
+<script>
+    export let data
+</script>
+
 <h1>Anasayfa</h1>
+
+{JSON.stringify(data)}
